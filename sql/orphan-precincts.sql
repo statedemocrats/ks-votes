2f362d5b4e758d8bcac7ffddef99cb953d419482
Peter Karman <peter.karman@gsa.gov>
2017-10-03 14:41:27 -0500

    common SQL statements for convenience

---
+++ sql/orphan-precincts.sql
@@ -0,0 +1,1 @@
+select p.name,p.county_id,c.name from precincts as p, counties as c where county_id=c.id and p.id > 4244 and p.id not in (select precinct_id from census_precincts) and p.census_tract_id is null order by c.name, p.name

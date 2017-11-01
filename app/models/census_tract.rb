2ac1f5fa56c0c881cd0899b9b205e306f5caf76c
Peter Karman <karpet@peknet.com>
2017-11-01 15:50:56 -0500

    VTD may be alphanumeric

--- app/models/census_tract.rb
+++ app/models/census_tract.rb
@@ -16,7 +16,7 @@ class CensusTract < ApplicationRecord
   enum reason: { census: 0, curated: 1, sos: 2 }
 
   def self.find_by_vtd_2012(vtd)
-    m = vtd.match(/^20(\d\d\d)(\d\d\d\d\d\d)$/)
+    m = vtd.match(/^20(\d\d\d)(\w\w\w\w\w\w)$/)
     includes(:county).where(counties: {fips: m[1]}).where(vtd_code: m[2]).first
   end
 end

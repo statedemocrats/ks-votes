9230c63ac52fe426a4a8585421bce6e2d9e94911
Peter Karman <karpet@peknet.com>
2017-11-21 23:34:49 -0600

    tweek sql output

--- sql/results_for_precinct.sql
+++ sql/results_for_precinct.sql
@@ -1,4 +1,4 @@
-select candidate_id,office_id,election_id,votes,e.name,o.name
-from results as r, elections as e, offices as o
+select votes,c.name,e.name,o.name,checksum
+from results as r, elections as e, offices as o, candidates as c
 where r.precinct_id = {0}
-and e.id=r.election_id and o.id=r.office_id
+and e.id=r.election_id and o.id=r.office_id and c.id=r.candidate_id
